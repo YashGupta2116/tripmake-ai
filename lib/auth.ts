3558103607e27d1e6808de4df2e12080097109af
YashGupta2116 <yashgupta2116@gmail.com>
2026-01-08 18:49:21 +0530

    setup better-auth for authentication and integrated it with prisma

---
+++ lib/auth.ts
@@ -0,0 +1,28 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+import prisma from "@/lib/prisma";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: "",
+      clientSecret: "",
+    },
+    google: {
+      clientId: "",
+      clientSecret: "",
+    },
+  },
+  plugins: [nextCookies()],
+  session: {
+    expiresIn: 60 * 60 * 24 * 2,
+  },
+});
